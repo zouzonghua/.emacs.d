ac4803435a67e7ce3b572b2c3b5ffaa6d98f7f76
zouzonghua <zouzonghua.cn@gmail.com>
2022-05-30 20:59:18 +0800

    feat: add measure startup time.

--- init.el
+++ init.el
@@ -17,7 +17,7 @@
 
 
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
-;(require 'init-benchmarking) ;; Measure startup time
+(require 'init-benchmarking) ;; Measure startup time
 
 (defconst *is-a-mac* (eq system-type 'darwin))
 
