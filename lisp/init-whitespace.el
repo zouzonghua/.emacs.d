5eed595c911c3d4ff9285a40f177cb2802988781
zouzonghua <zouzonghua.cn@gmail.com>
2022-06-01 14:08:53 +0800

    feat: update config

--- lisp/init-whitespace.el
+++ lisp/init-whitespace.el
@@ -3,16 +3,14 @@
 ;;; Code:
 
 (setq-default show-trailing-whitespace nil)
-
 
 ;;; Whitespace
-
-(defun sanityinc/show-trailing-whitespace ()
+(defun show-trailing-whitespace ()
   "Enable display of trailing whitespace in this buffer."
   (setq-local show-trailing-whitespace t))
 
 (dolist (hook '(prog-mode-hook text-mode-hook conf-mode-hook))
-  (add-hook hook 'sanityinc/show-trailing-whitespace))
+  (add-hook hook 'show-trailing-whitespace))
 
 
 (use-package whitespace-cleanup-mode)
@@ -21,7 +19,7 @@
   (diminish 'whitespace-cleanup-mode))
 
 (global-set-key [remap just-one-space] 'cycle-spacing)
-
+
 
 (provide 'init-whitespace)
 ;;; init-whitespace.el ends here
